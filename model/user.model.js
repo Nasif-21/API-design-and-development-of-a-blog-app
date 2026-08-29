ec64c9b1bf36e674bc5dd39976cf43d0332635c3
Nasif <nasifshitab01@gmail.com>
2026-08-29 22:05:31 +0600

    User table schema

---
+++ model/user.model.js
@@ -0,0 +1,53 @@
+import sequelize from "../config/db.js";
+import { DataTypes } from "sequelize";
+
+
+const User=sequelize.define('Users',{
+    id:{
+        type:DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true,
+    },
+    firstName:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    lastName:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    email:{
+        type:DataTypes.STRING,
+        unique:true,
+        allowNull:false,
+    },
+    password:{
+        type:DataTypes.STRING,
+        allowNull:true,
+    },
+    isActive:{
+        type:DataTypes.BOOLEAN,
+        allowNull:false,
+        defaultValue:true,
+    },
+    role:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        defaultValue:"user"
+
+    },
+},
+{
+    tableName:'users',
+    timestamps:true,
+    createdAt:'createdAt',
+    updatedAt:'updatedAt',
+}
+);
+
+
+
+export default User
+
+
+
