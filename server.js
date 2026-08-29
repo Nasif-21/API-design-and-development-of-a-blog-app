be05ca749834a6b1064d7a0925dbeab20552428c
Nasif <nasifshitab01@gmail.com>
2026-08-29 21:58:41 +0600

    Server configuration

---
+++ server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import sequelize from "./config/db.js";
+dotenv.config();
+import app from "./app.js";
+import './model/association.js';
+
+
+const PORT=process.env.PORT||5001;
+
+await sequelize.authenticate();
+await sequelize.sync({alter:true})
+console.log(`Database start at port ${process.env.DB_PORT}`)
+app.listen(PORT,()=>{
+    console.log(`Server starts at port ${PORT} `)
+});
+
