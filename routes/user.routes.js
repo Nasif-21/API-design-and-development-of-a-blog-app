8b204ed576c3499c37b8f5371e26b617dea1b557
Nasif <nasifshitab01@gmail.com>
2026-09-03 22:10:10 +0600

    Added user action endpoints

---
+++ routes/user.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router=Router();
+
+// router.get("/api/users/profile")
+// router.put("/api/users/profile/update")
+// router.patch("/api/users/password")
+// router.post("/api/blogs/create")
+// router.put("/api/blogs/update/:id")
+// router.delete("/api/blogs/delete/:id")
+
+
+
+export default router;
