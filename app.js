b4dc13021c4074f9da09270102a2c6b17ab582a2
Nasif <nasifshitab01@gmail.com>
2026-09-03 22:03:33 +0600

    Register server check and guest router inside app

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express"
 import path from "path"
+import router from "./routes/server.routes.js";
+import guestRouter from './routes/guest.routes.js'
 
 const app=express();
 
-app.get("/");
+app.use(express.json());
+
+app.use("/",router)
+app.use("/",guestRouter)
 
 export default app;
 
