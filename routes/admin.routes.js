4969679cf933c44ef3b9bb4ed55210ae96786452
Nasif <nasifshitab01@gmail.com>
2026-09-03 22:09:36 +0600

    Define admin action endpoints

---
+++ routes/admin.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+const router=Router();
+
+// router.post("/api/auth/register");
+// router.post("/api/auth/login");
+// router.get("/api/users")
+// router.get("/api/users/:id")
+// router.patch("/api/users/:id/status")
+// router.get("/api/users/profile")
+// router.put("/api/users/profile/update")
+// router.patch("/api/users/password")
+// router.get("/api/blogs")
+// router.get("/api/blogs/:id")
+// router.post("/api/blogs/create")
+// router.put("/api/blogs/update/:id")
+// router.delete("/api/blogs/delete/:id")
+
+
+
+export default router;
