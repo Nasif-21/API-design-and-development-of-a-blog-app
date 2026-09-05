bd7bcc4c0b1b5aa648e90aea44805d1a7acccfac
Nasif <nasifshitab01@gmail.com>
2026-09-05 22:13:08 +0600

    Developed admin router actions

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { authMiddleware,isAdmin } from "../middleware/auth.middleware.js";
-import { viewAllUsers } from "../controller/admin.controller.js";
+import { viewAllUsers,findUserbyId,statusUpdate } from "../controller/admin.controller.js";
 
 const router=Router();
 
 
- router.get("/api/users",authMiddleware,isAdmin,viewAllUsers)
-// router.get("/api/users/:id")
-// router.patch("/api/users/:id/status")
+router.get("/api/users",authMiddleware,isAdmin,viewAllUsers)
+router.get("/api/users/:id",authMiddleware,isAdmin,findUserbyId)
+router.patch("/api/users/:id/status",authMiddleware,isAdmin,statusUpdate)
 // router.get("/api/users/profile")
 // router.put("/api/users/profile/update")
 // router.patch("/api/users/password")
