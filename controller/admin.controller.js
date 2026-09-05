8b7ae9b1360d2bc719c62452357802a43d886900
Nasif <nasifshitab01@gmail.com>
2026-09-05 22:11:06 +0600

    Added user status  update functions

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -1,5 +1,6 @@
 import User from '../model/user.model.js'
 import Blog from '../model/blog.model.js'
+import { where } from 'sequelize';
 
 export const viewAllUsers=async(req,res)=>{
     
@@ -33,8 +34,36 @@ export const findUserbyId=async(req,res)=>{
         })
     }
     res.status(201).json({
-        message:"Lists of Users",
+        message:"User found",
         data:findUser
         
     })
+}
+
+export const statusUpdate=async(req,res)=>{
+    const {isActive}=req.body
+    const findId=req.params.id;
+
+    const findUser=await User.findByPk(findId,{attributes:{exclude:["password"]}});
+
+    if(!findUser)
+    {
+        return res.status(404).json({
+            message:"User not found"
+        })
+    }
+    if(isActive!==0 && isActive!==1 )
+    {
+        return res.status(409).json({
+            message:"Set user status either 1 or 0"
+        })
+    }
+
+    await findUser.update({isActive:Boolean(isActive)})
+    res.status(200).json({
+        message:"User status updated",
+        data:findUser
+    })
+
+    
 }
