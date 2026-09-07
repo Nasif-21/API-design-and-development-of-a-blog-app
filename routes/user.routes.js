d4bc6f3a9ab453e77421b946c0ca7c59d6b2330b
Nasif <nasifshitab01@gmail.com>
2026-09-07 22:04:32 +0600

    Create user controller register in router

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+import { viewOwnProfile,updateOwnProfile,updateOwnPassword,createBlogs,updateBlogId,deleteBlogbyId } from "../controller/user.controller.js";
 
 const router=Router();
 
-// router.get("/api/users/profile")
-// router.put("/api/users/profile/update")
-// router.patch("/api/users/password")
-// router.post("/api/blogs/create")
-// router.put("/api/blogs/update/:id")
-// router.delete("/api/blogs/delete/:id")
+router.get("/api/users/profile",authMiddleware,viewOwnProfile)
+router.put("/api/users/profile/update",authMiddleware,updateOwnProfile)
+router.patch("/api/users/password",authMiddleware,updateOwnPassword)
+router.post("/api/blogs/create",authMiddleware,createBlogs)
+router.put("/api/blogs/update/:id",authMiddleware,updateBlogId)
+router.delete("/api/blogs/delete/:id",authMiddleware,deleteBlogbyId)
 
 
 
