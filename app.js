9a927b5d95f7734b7394b1313d750b00ecc22d25
Nasif <nasifshitab01@gmail.com>
2026-09-07 22:03:00 +0600

    Fix routing position to avoid classic express bug

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from "express"
 import path from "path"
 import router from "./routes/server.routes.js";
 import guestRouter from './routes/guest.routes.js';
-import adminRouter from './routes/admin.routes.js'
+import adminRouter from './routes/admin.routes.js';
+import userRouter from './routes/user.routes.js';
 
 const app=express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/",router)
 app.use("/",guestRouter)
+app.use("/",userRouter)
 app.use("/",adminRouter)
 
 export default app;
